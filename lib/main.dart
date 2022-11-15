ceb6d7c9279e26fe292f32f9be5aa79ebd96f29d
Tarun Kumar <tarun@pantelwar.com>
2022-11-15 17:57:42 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 2,
+          title: const Text('Flutter Catalog App'),
+        ),
+      ),
+    );
+  }
+}
